a080e68310833f1fb4dbf372f38de4134cf0ae69
YangBLUD <2433171628@qq.com>
2023-05-17 10:26:11 +0800

    Update App.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -29,57 +29,41 @@
         width="200px"
         style="height: calc(100vh - 60px); background-color: rgb(238, 241, 246)"
       >
-        <el-menu :default-openeds="['1', '3']" style="overflow-x: hidden">
-          <el-submenu index="1">
-            <template slot="title"
-              ><i class="el-icon-message"></i>导航一</template
-            >
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-submenu index="2">
-            <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="2-1">选项1</el-menu-item>
-              <el-menu-item index="2-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="2-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-submenu index="3">
-            <template slot="title"
-              ><i class="el-icon-setting"></i>导航三</template
-            >
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="3-1">选项1</el-menu-item>
-              <el-menu-item index="3-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="3-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="3-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-        </el-menu>
+        <el-row class="tac">
+          <el-col>
+            <el-menu default-active="2" class="el-menu-vertical-demo">
+              <el-submenu index="1">
+                <template slot="title">
+                  <i class="el-icon-s-home"></i>
+                  <span>首页</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="1-1">热门领域</el-menu-item>
+                  <el-menu-item index="1-2">热门学者</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+              <el-menu-item index="2">
+                <i class="el-icon-search"></i>
+                <span slot="title">搜索</span>
+              </el-menu-item>
+              <el-submenu index="2">
+                <template slot="title">
+                  <i class="el-icon-user-solid"></i>
+                  <span slot="title">我的</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="2-1">收藏</el-menu-item>
+                  <el-menu-item index="2-2">信息</el-menu-item>
+                  <el-menu-item index="2-3">消息</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+              <el-menu-item index="4">
+                <i class="el-icon-s-tools"></i>
+                <span slot="title">设置</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
       </el-aside>
 
       <el-main style="height: calc(100vh - 60px)">
