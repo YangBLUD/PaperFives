7788fd35b086fff3e97c190ea6506bde59826452
YangBLUD <2433171628@qq.com>
2023-05-21 14:25:31 +0800

    Update User.vue

--- vue-project/src/views/User.vue
+++ vue-project/src/views/User.vue
@@ -1,6 +1,7 @@
 <template>
     <el-row>
         <el-col :span="8">
+            <!-- 个人名片 -->
             <h3 class="page-title">我</h3>
             <el-card class="box-card">
                 <div class="user">
@@ -11,6 +12,7 @@
                     </div>
                 </div>
             </el-card>
+
             <h3 class="page-title">我的关注</h3>
             <!-- 关注列表 -->
             <el-row :gutter="20" class="follow-list">
@@ -24,9 +26,9 @@
                                 </div>
                                 <!-- 用户名 -->
                                 <div class="card_name">{{ item.username }}</div>
+                                <!-- 关注操作 -->
                                 <el-button type="danger" size="small" @click="removeFollow(index)">取消关注</el-button>
                             </div>
-                            <!-- 关注操作 -->
                         </el-card>
                     </div>
                 </template>
@@ -41,6 +43,7 @@
 
         <el-col :span="16" style="padding-left: 30px">
             <h3 class="page-title">我的论文</h3>
+            <!-- 论文列表 -->
             <el-row :gutter="20" class="paper-list">
                 <template v-if="paperList.length > 0">
                     <el-col :span="24" v-for="(item, index) in paperList" :key="index">
@@ -61,14 +64,19 @@
 
             <h3 class="page-title">我的统计</h3>
             <div class="graph">
-                <el-card style="height: 280px;"></el-card>
-                <el-card style="height: 280px;"></el-card>
+                <el-card style="height: 330px">
+                    <div class="echart" id="mychart1" :style="myChartStyle"></div>
+                </el-card>
+                <el-card style="height: 330px">
+                    <div class="echart" id="mychart2" :style="myChartStyle"></div>
+                </el-card>
             </div>
         </el-col>
     </el-row>
 </template>
   
 <script>
+import * as echarts from 'echarts'
 export default {
     data() {
         return {
@@ -85,7 +93,7 @@ export default {
                 { username: "张三", avatar: "https://picsum.photos/id/1028/1000/1000" },
             ],
             paperList: [
-            { username: "张三四", avatar: "https://picsum.photos/id/1028/1000/1000" },
+                { username: "张三四", avatar: "https://picsum.photos/id/1028/1000/1000" },
                 { username: "李四", avatar: "https://picsum.photos/id/1019/1000/1000" },
                 { username: "王五", avatar: "https://picsum.photos/id/1027/1000/1000" },
                 { username: "赵六", avatar: "https://picsum.photos/id/1026/1000/1000" },
@@ -95,16 +103,173 @@ export default {
                 { username: "张三", avatar: "https://picsum.photos/id/1028/1000/1000" },
                 { username: "张三", avatar: "https://picsum.photos/id/1028/1000/1000" },
                 { username: "张三", avatar: "https://picsum.photos/id/1028/1000/1000" },
-            ]
+            ],
+            xData: ["1990s", "2000s", "2010s", "2020s"], //横坐标
+            yData: [23, 24, 18, 25], //数据
+            myChartStyle: { float: "left", width: "90%", height: "280px" } //图表样式
         };
+    },
+    mounted() {
+        this.initEcharts();
     },
     methods: {
         removeFollow(index) {
             this.followList.splice(index, 1);
         },
-    },
+        initEcharts() {
+            // 基本柱状图
+            const option1 = {
+                title: {
+                    // 设置饼图标题，位置设为顶部居中
+                    text: "论文发表记录",
+                    top: "0%",
+                    left: "center",
+                    textStyle: {
+                        color: "#333",
+                        fontWeight: "bold",
+                        fontFamily: "Microsoft YaHei"
+                    }
+                },
+                grid: {
+                    top: 80,
+                    left: 80,
+                    right: 50
+                },
+                xAxis: {
+                    data: this.xData,
+                    axisLine: {
+                        lineStyle: {
+                            color: "#999"
+                        }
+                    },
+                    axisLabel: {
+                        color: "#666",
+                        margin: 10
+                    },
+                    axisTick: {
+                        show: false
+                    }
+                },
+                yAxis: {
+                    axisLine: {
+                        lineStyle: {
+                            color: "#999"
+                        }
+                    },
+                    axisLabel: {
+                        color: "#666",
+                        margin: 10
+                    },
+                    splitLine: {
+                        show: true,
+                        lineStyle: {
+                            type: "dashed"
+                        }
+                    }
+                },
+                color: ["#2ec7c9"],
+                series: [
+                    {
+                        type: "bar", //形状为柱状图
+                        data: this.yData,
+                        barWidth: 30,
+                        itemStyle: {
+                            color: "#2ec7c9"
+                        }
+                    }
+                ]
+            };
+
+            const option2 = {
+                legend: {
+                    // 图例
+                    data: ["OS", "OO", "DB", "SE", "AI"],
+                    right: "10%",
+                    top: "50%",
+                    orient: "vertical",
+                    textStyle: {
+                        color: "#666",
+                        fontSize: 14
+                    },
+                    itemWidth: 16,
+                    itemHeight: 16,
+                    itemGap: 20
+                },
+                title: {
+                    // 设置饼图标题，位置设为顶部居中
+                    text: "研究领域分布",
+                    top: "0%",
+                    left: "center",
+                    textStyle: {
+                        color: "#333",
+                        fontWeight: "bold",
+                        fontFamily: "Microsoft YaHei"
+                    }
+                },
+                series: [
+                    {
+                        type: "pie",
+                        radius: ["50%", "70%"],
+                        center: ["50%", "55%"],
+                        label: {
+                            show: true,
+                            fontSize: 16,
+                            formatter: "{b} {d}%"
+                        },
+                        labelLine: {
+                            length: 5,
+                            length2: 10,
+                            lineStyle: {
+                                width: 1
+                            }
+                        },
+                        data: [
+                            {
+                                value: 463,
+                                name: "OS"
+                            },
+                            {
+                                value: 395,
+                                name: "OO"
+                            },
+                            {
+                                value: 157,
+                                name: "DB"
+                            },
+                            {
+                                value: 149,
+                                name: "SE"
+                            },
+                            {
+                                value: 147,
+                                name: "AI"
+                            }
+                        ],
+                        itemStyle: {
+                            borderWidth: 10,
+                            borderColor: "#fff"
+                        }
+                    }
+                ]
+            };
+            const myChart1 = echarts.init(document.getElementById("mychart1"));
+            myChart1.setOption(option1);
+            //随着屏幕大小调节图表
+            window.addEventListener("resize", () => {
+                myChart1.resize();
+            });
+
+            const myChart2 = echarts.init(document.getElementById("mychart2"));
+            myChart2.setOption(option2);
+            //随着屏幕大小调节图表
+            window.addEventListener("resize", () => {
+                myChart2.resize();
+            });
+        }
+    }
 };
 </script>
+
   
 <style lang="less" scoped>
 .user {
@@ -191,7 +356,7 @@ export default {
 }
 
 .paper-list {
-    height: 400px;
+    height: 350px;
     overflow-y: auto;
 }
 
