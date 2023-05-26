86bafd863d25b58e82ff07a87046a4d27064bf6b
YangBLUD <2433171628@qq.com>
2023-05-26 23:21:50 +0800

    Update User.vue

--- vue-project/src/views/User.vue
+++ vue-project/src/views/User.vue
@@ -1,4 +1,5 @@
 <template>
+    <!-- 页面框架 -->
     <el-row class="border">
         <el-col :span="8">
             <!-- 个人名片 -->
@@ -18,17 +19,20 @@
                 <el-tabs v-model="activeName" @tab-click="handleClick" style="height: auto; width: auto;">
                     <el-tab-pane name="first">
                         <span slot="label" style="font-size:20px; font-weight: 700;">关注</span>
+                        <!-- 有关注 -->
                         <template v-if="followeeList.length > 0">
-                            <div v-for="(item, index) in newList" :key="index">
+                            <div v-for="(item, index) in      newList     " :key="index">
                                 <el-card shadow="hover" class="follow-item">
                                     <div class="follow-info">
                                         <!-- 用户头像 -->
-                                        <div @click="gotoProfile(item.uid, item.isFollowed)">
+                                        <div @click="gotoProfile(item.uid)">
                                             <el-avatar :src="'http://81.70.161.76:5000' + item.avatar" size="90"
-                                                :border="false"></el-avatar>
+                                                :border="false" class="avatar-box"></el-avatar>
                                         </div>
+
                                         <!-- 用户名 -->
-                                        <div class="card_name" @click="gotoProfile(item.uid)">{{ item.username }}</div>
+                                        <div class="card_name" @click="gotoProfile(item.uid)">{{
+                                            item.username }}</div>
                                         <!-- 关注操作 -->
                                         <div class="follow-tag">
                                             <el-button v-if="item.isFollowed" type="danger" size="small"
@@ -44,6 +48,7 @@
                                 </el-card>
                             </div>
                         </template>
+                        <!-- 无关注 -->
                         <template v-else>
                             <el-col :span="24">
                                 <br><br>
@@ -51,38 +56,50 @@
                             </el-col>
                         </template>
                     </el-tab-pane>
+
+                    <!-- 粉丝列表 -->
                     <el-tab-pane name="second">
                         <span slot="label" style="font-size:20px; font-weight: 700;">粉丝</span>
+                        <!-- 有粉丝 -->
                         <template v-if="followerList.length > 0">
-                            <div v-for="(item, index) in followerList" :key="index">
+                            <div v-for="(     item, index     ) in      followerList     " :key="index">
                                 <el-card shadow="hover" class="follow-item">
                                     <div class="follow-info">
                                         <!-- 用户头像 -->
                                         <div @click="gotoProfile(item.uid)">
                                             <el-avatar :src="'http://81.70.161.76:5000' + item.avatar" size="90"
-                                                :border="false"></el-avatar>
+                                                :border="false" class="avatar-box"></el-avatar>
                                         </div>
                                         <!-- 用户名 -->
-                                        <div class="card_name" @click="gotoProfile(item.uid)">{{ item.username }}</div>
+                                        <div class="card_name" @click="gotoProfile(item.uid)">{{
+                                            item.username }}</div>
                                     </div>
                                 </el-card>
                             </div>
+                        </template>
+                        <!-- 无粉丝 -->
+                        <template v-else>
+                            <el-col :span="24">
+                                <br><br>
+                                <el-empty description="无粉丝" :image-size="250"></el-empty>
+                            </el-col>
                         </template>
                     </el-tab-pane>
                 </el-tabs>
             </el-row>
         </el-col>
 
+        <!-- 论文列表 -->
         <el-col :span="16" style="padding-left: 30px">
             <h3 class="page-title">我的论文</h3>
             <!-- 论文列表 -->
             <el-row :gutter="20" class="paper-list">
                 <template v-if="paperList.length > 0">
-                    <el-col :span="24" v-for="(item, index) in paperList" :key="index">
+                    <el-col :span="24" v-for="(     item, index     ) in      paperList     " :key="index">
                         <el-card shadow="hover" class="papaer-item">
                             <!-- 查看操作 -->
                             <div class="paper-action">
-                                <el-button type="primary" size="small" @click="">查看论文</el-button>
+                                <el-button type="primary" size="small" @click=" ">查看论文</el-button>
                             </div>
                         </el-card>
                     </el-col>
@@ -94,6 +111,7 @@
                 </template>
             </el-row>
 
+            <!-- 我的统计 -->
             <h3 class="page-title">我的统计</h3>
             <div class="graph">
                 <el-carousel :interval="4000" type="card" height="330px" style="width: 1000px;">
@@ -123,6 +141,7 @@ export default {
             followeeList: [],
             newList: [],
             paperList: [],
+            followeeTag: false,
             xData: ["1990s", "2000s", "2010s", "2020s"], //横坐标
             yData: [23, 24, 18, 25], //数据
             myChartStyle: { float: "left", width: "90%", height: "280px" }, //图表样式
@@ -205,25 +224,42 @@ export default {
             // pair[0].classList.add("hidden")
             // pair[1].classList.remove("hidden"))
         },
+        async isFollowee(id) {
+            await this.$http.get('api/v1/users/favorite/isfollowee', {
+                params: {
+                    uid: id
+                }
+            })
+                .then(res => {
+                    console.log(res);
+                    this.followeeTag = res.data.data.value
+                }).catch(err => {
+                    console.log(err);
+                })
+        },
         async followUser(id, index) {
             this.newList[index].isFollowed = !this.newList[index].isFollowed;
             await this.$http.post('api/v1/users/favorite/follow', {
                 uid: id
             })
                 .then(res => {
-                    // console.log(res);
+                    console.log(res);
                 }).catch(err => {
                     console.log(err);
                 })
         },
-        gotoProfile(id) {
+        async gotoProfile(id) {
+            await this.isFollowee(id)
+            console.log(this.followeeTag)
             this.$router.push({
                 path: '/visitor',
                 query: {
-                    uid: id
+                    uid: id,
+                    status: this.followeeTag
                 }
             })
         },
+
         initEcharts() {
             // 基本柱状图
             const option1 = {
@@ -385,6 +421,30 @@ export default {
     max-width: max-content;
 }
 
+.follow-item:hover {
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.5);
+}
+
+
+.avatar-box {
+    position: relative;
+    transition: all 0.3s ease-in-out;
+    /* 添加过渡效果 */
+    transform: scale(1);
+    /* 设置默认的缩放比例 */
+    opacity: 0.8;
+    /* 设置默认的透明度 */
+}
+
+.avatar-box:hover {
+    cursor: pointer;
+    position: relative;
+    transform: scale(1.2);
+    /* 鼠标移动到头像上时，放大 20% */
+    opacity: 1;
+    /* 鼠标移动到头像上时，使透明度设置为 1 */
+}
+
 .user {
     display: flex;
     align-items: center;
@@ -454,6 +514,10 @@ export default {
         margin-left: 20px;
         width: 50px;
     }
+
+    .card_name:hover {
+        cursor: pointer;
+    }
 }
 
 ::v-deep .el-card__body {
@@ -498,6 +562,5 @@ export default {
     display: flex;
     justify-content: flex-end;
     align-items: center;
-}
-</style>
+}</style>
   
