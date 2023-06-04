c6746d62f77ce14bfdaae19df99f0ee56c18a2b0
Lord Turmoil <tony-turmoil@qq.com>
2023-06-04 21:15:06 +0800

    feat: init message view

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -4,27 +4,29 @@ import Login from '../components/start/Login.vue'
 import Register from '../components/start/Register.vue'
 import Home from '../components/home.vue'
 import mainPage from '../components/mainPage.vue'
-import advSearch from'../components/search/advSearch.vue'
+import advSearch from '../components/search/advSearch.vue'
 import User from '../views/User.vue'
 import Info from '../views/UserInfo.vue'
 import Visitor from '../views/Visitor.vue'
 import Paper from '../views/Paper.vue'
+import Message from '../views/MessageView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', redirect:'/login' },
+  { path: '/', redirect: '/login' },
   {
-    path:'/home',
-    component:Home,
-    redirect:'/main',
-    children:[
-      { path:'/main',component:mainPage},
-      { path:'/advsearch',component:advSearch},
-      { path:'/user',component:User},
-      { path:'/info',component:Info},
-      { path:'/visitor',component:Visitor},
-      { path:'/paper',component:Paper}
+    path: '/home',
+    component: Home,
+    redirect: '/main',
+    children: [
+      { path: '/main', component: mainPage },
+      { path: '/advsearch', component: advSearch },
+      { path: '/user', component: User },
+      { path: '/info', component: Info },
+      { path: '/visitor', component: Visitor },
+      { path: '/paper', component: Paper },
+      { path: '/message', component: Message }
     ]
   },
   { path: '/login', component: Login },
---
+++ vue-project/src/views/MessageView.vue
@@ -0,0 +1,5 @@
+<template></template>
+
+<style>
+@import '../assets/css/message.css';
+</style>
