e56ef02c676396162b312ee34e8f354f62146ed6
YangBLUD <2433171628@qq.com>
2023-06-05 00:42:47 +0800

    Update Visitor.vue

--- vue-project/src/views/Visitor.vue
+++ vue-project/src/views/Visitor.vue
@@ -2,7 +2,7 @@
     <el-row class="border">
         <!-- 个人名片 -->
         <div style="display: flex; justify-content: center; align-items: center; padding-top: 30px;">
-            <el-card class="box-card" style="margin: 0 auto;">
+            <el-card class="box-card" style="margin: 0 auto; text-align: left;">
                 <el-col :span="22">
                     <div class="user">
                         <img :src="'http://81.70.161.76:5000' + this.userProfile.avatar" />
@@ -29,7 +29,6 @@
                                 <p v-if="this.userAttr.motto" class="motto">{{ this.userAttr.motto }}</p>
                                 <p v-else class="motto">&nbsp;</p>
                             </template>
-                            <div>{{ this.paperList.length }}</div>
                         </div>
                     </div>
                 </el-col>
@@ -45,115 +44,119 @@
             </el-card>
         </div>
 
-
         <!-- 论文列表 -->
-        <el-row :gutter="20" class="paper-list">
-            <template v-if="paperList.length > 0">
-                <el-col :span="12" v-for="(item, index) in paperList" :key="index">
-                    <el-card shadow="hover" class="papaer-item">
-                        <!-- 查看操作 -->
-                        <div class="paper-action">
-                            <el-button type="primary" size="small" @click="">查看论文</el-button>
+        <el-col :span="12" class="left-col">
+            <template v-if="this.truePaperList.length > 0">
+                <el-col v-for="(item, index) in paperList" :key="index">
+                    <el-card v-if="item.pid === hot" shadow="hover" style="height: 500px;">
+                        <span class="paper_name" @click="gotoPaper(item.pid)">{{ item.attr.title
+                        }}</span>
+                        <div class="content">
+                            <div class="authors">
+                                <span v-for="(author, index) in item.authors" class="author-name">
+                                    <span @click="gotoProfile(author.uid)">{{ author.name }}</span>
+                                    <span v-if="index < item.authors.length - 1" style="color: #A0A0A0; font-size: 14px"> /
+                                    </span>
+                                </span>
+                                <span class="publish-year"> · {{ item.attr.publish_date }}</span>
+                            </div>
+                            <div>
+                                <span v-for="(keyword) in item.attr.keywords" class="abstract">
+                                    ●{{ keyword }}&nbsp;&nbsp;&nbsp;
+                                </span>
+                            </div>
+                            <div>
+                                <span class="abstract">{{ item.attr.abstract | ellipsis }}</span>
+                            </div>
+                            <div class="citation-count">
+                                <span>{{ item.stat.cites }}&nbsp;被引用</span>
+                                <span>&nbsp;·&nbsp;{{ item.stat.downloads }}&nbsp;被收藏</span>
+                                <span>&nbsp;·&nbsp;{{ item.stat.favorites }}&nbsp;下载量</span>
+                                <span>&nbsp;·&nbsp;{{ item.stat.clicks }}&nbsp;点击量</span>
+                            </div>
                         </div>
                     </el-card>
                 </el-col>
             </template>
             <template v-else>
-                <el-col :span="12">
-                    <el-empty description="无论文数据" :image-size="250"></el-empty>
-                </el-col>
+                <el-empty description="无论文数据" :image-size="250"></el-empty>
             </template>
-            <template v-if="paperList.length > 0">
-                <el-col span="12" v-for="(item, index) in paperList" :key="index">
-                    <el-card shadow="hover" class="paper-item">
-                        <div class="wrapper">
-                            <div v-show="showCard[index]">
-                                <div class="paper-item-title">
-                                    <span class="paper_name" @click="gotoPaper(item.pid)">{{ item.attr.title }}</span>
-                                    <el-button class="shrink" icon="el-icon-arrow-left"
-                                        @click="$set(showCard, index, false)" size="mini"></el-button>
-                                </div>
-                                <div class="content">
-                                    <div class="authors">
-                                        <span v-for="(author, index) in item.authors" class="author-name">
-                                            <span @click="gotoProfile(author.uid)">{{ author.name }}</span>
-                                            <span v-if="index < item.authors.length - 1"
-                                                style="color: #A0A0A0; font-size: 14px"> / </span>
-                                        </span>
-                                        <span class="publish-year"> · {{ item.attr.publish_date }}</span>
+        </el-col>
+
+        <el-col :span="12" class="right-col">
+            <!-- 论文列表 -->
+            <el-row :gutter="20" class="paper-list">
+                <template v-if="truePaperList.length > 0">
+                    <el-col v-for="(item, index) in truePaperList" :key="index">
+                        <el-card shadow="hover" class="paper-item">
+                            <div class="wrapper">
+                                <div v-show="showCard[index]">
+                                    <div class="paper-item-title">
+                                        <span class="paper_name" @click="gotoPaper(item.pid)">{{ item.attr.title
+                                        }}</span>
+                                        <el-button class="shrink" icon="el-icon-arrow-left"
+                                            @click="$set(showCard, index, false)" size="mini"></el-button>
                                     </div>
-                                    <div>
-                                        <span v-for="(keyword) in item.attr.keywords" class="abstract">
-                                            ●{{ keyword }}&nbsp;&nbsp;&nbsp;
-                                        </span>
+                                    <div class="content">
+                                        <div class="authors">
+                                            <span v-for="(author, index) in item.authors" class="author-name">
+                                                <span @click="gotoProfile(author.uid)">{{ author.name }}</span>
+                                                <span v-if="index < item.authors.length - 1"
+                                                    style="color: #A0A0A0; font-size: 14px"> / </span>
+                                            </span>
+                                            <span class="publish-year"> · {{ item.attr.publish_date }}</span>
+                                        </div>
+                                        <div>
+                                            <span v-for="(keyword) in item.attr.keywords" class="abstract">
+                                                ●{{ keyword }}&nbsp;&nbsp;&nbsp;
+                                            </span>
+                                        </div>
+                                        <div style="text-align:left;margin-top:10px;">
+                                            <span class="abstract">{{ item.attr.abstract | ellipsis }}</span>
+                                        </div>
+                                        <div class="citation-count">
+                                            <span>{{ item.stat.cites }}&nbsp;被引用</span>
+                                            <span>&nbsp;·&nbsp;{{ item.stat.downloads }}&nbsp;被收藏</span>
+                                            <span>&nbsp;·&nbsp;{{ item.stat.favorites }}&nbsp;下载量</span>
+                                            <span>&nbsp;·&nbsp;{{ item.stat.clicks }}&nbsp;点击量</span>
+                                        </div>
                                     </div>
-                                    <div style="text-align:left;margin-top:10px;">
-                                        <span class="abstract">{{ item.attr.abstract | ellipsis }}</span>
-                                    </div>
-                                    <div class="citation-count">
-                                        <span>{{ item.stat.cites }}&nbsp;被引用</span>
-                                        <span>&nbsp;·&nbsp;{{ item.stat.downloads }}&nbsp;被收藏</span>
-                                        <span>&nbsp;·&nbsp;{{ item.stat.favorites }}&nbsp;下载量</span>
-                                        <span>&nbsp;·&nbsp;{{ item.stat.clicks }}&nbsp;点击量</span>
+                                    <div v-show="!showCard[index]">
+                                        {{ item.attr.title }}
                                     </div>
                                 </div>
                                 <div v-show="!showCard[index]">
-                                    {{ item.attr.title }}
+                                    <div class="paper-content">
+                                        <span class="paper_name_init" @click="gotoPaper(item.pid)">{{ item.attr.title
+                                        }}&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                                        <i class="el-icon-medal-1" style="font-size: 2em; color: #FFB90F;"></i>
+                                        <el-button icon="el-icon-view" size="mini" circle
+                                            @click="$set(showCard, index, true)"></el-button>
+                                    </div>
                                 </div>
                             </div>
-                            <div v-show="!showCard[index]">
-                                <div class="paper-content">
-                                    <span class="paper_name_init" @click="gotoPaper(item.pid)">{{ item.attr.title
-                                    }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                                    <i class="el-icon-medal-1" style="font-size: 2em; color: #FFB90F;"></i>
-                                    <el-button v-if="item.status === 0" type="success" size="normal" class="status"
-                                        icon="el-icon-edit">
-                                        草稿
-                                    </el-button>
-                                    <el-button v-else-if="item.status === 1" type="success" size="normal" class="status"
-                                        icon="el-icon-edit">
-                                        草稿
-                                    </el-button>
-                                    <el-button v-else-if="item.status === 2" type="warning" size="normal" class="status"
-                                        icon="el-icon-s-check">
-                                        审核中
-                                    </el-button>
-                                    <el-button v-else-if="item.status === 3" type="warning" size="normal" class="status"
-                                        icon="el-icon-s-check">
-                                        草稿
-                                    </el-button>
-                                    <el-button v-else-if="item.status === 4" type="danger" size="normal" class="status"
-                                        icon="el-icon-error">
-                                        已驳回
-                                    </el-button>
-                                    <el-button v-else-if="item.status === 5" type="primary" size="normal" class="status"
-                                        icon="el-icon-success">
-                                        已发表
-                                    </el-button>
-                                    <el-button icon="el-icon-view" size="mini" circle
-                                        @click="$set(showCard, index, true)"></el-button>
-                                </div>
-                            </div>
-                        </div>
-                    </el-card>
-                </el-col>
-            </template>
-            <template v-else>
-                <el-col :span="12">
-                    <el-empty description="无论文数据" :image-size="250"></el-empty>
-                </el-col>
-            </template>
-        </el-row>
+                        </el-card>
+                    </el-col>
+                </template>
+                <template v-else>
+                    <el-col>
+                        <el-empty description="无论文数据" :image-size="250"></el-empty>
+                    </el-col>
+                </template>
+            </el-row>
+        </el-col>
 
         <!-- 图表部分 -->
-        <div class="graph">
-            <el-card style="height: 330px">
-                <div class="echart" id="mychart1" :style="myChartStyle"></div>
-            </el-card>
-            <el-card style="height: 330px">
-                <div class="echart" id="mychart2" :style="myChartStyle"></div>
-            </el-card>
-        </div>
+        <el-col :span="24">
+            <div class="graph">
+                <el-card style="height: 330px">
+                    <div class="echart" id="mychart1" :style="myChartStyle"></div>
+                </el-card>
+                <el-card style="height: 330px">
+                    <div class="echart" id="mychart2" :style="myChartStyle"></div>
+                </el-card>
+            </div>
+        </el-col>
     </el-row>
 </template>
   
@@ -166,7 +169,10 @@ export default {
             userProfile: {},
             userAttr: {},
             paperList: [],
+            truePaperList: [],
             paperNum: 0,
+            showCard: [],
+            hot: 0,
             xData: ["1990s", "2000s", "2010s", "2020s"], //横坐标
             yData: [23, 24, 18, 25], //数据
             myChartStyle: { float: "left", width: "90%", height: "280px" }, //图表样式
@@ -176,9 +182,12 @@ export default {
     mounted() {
         this.$nextTick(() => {
             this.initEcharts();
+            this.getPaperlist();
             this.getUserProfile();
             this.isFollowee();
-            this.getPaperlist();
+            this.paperList.forEach(() => {
+                this.$set(this.showCard, this.showCard.length, false);
+            });
         });
     },
     methods: {
@@ -242,9 +251,33 @@ export default {
                     console.log(res);
                     this.paperList = res.data.data.papers;
                     this.paperNum = res.data.data.total;
+                    this.truePaperList = this.paperList.filter(item => item.status === 5);
+                    this.hot = res.data.data.hot;
                 }).catch(err => {
                     console.log(err);
                 })
+        },
+        async gotoPaper(id) {
+            this.$router.push({
+                path: '/paper',
+                query: {
+                    pid: id,
+                }
+            })
+        },
+        async gotoProfile(id) {
+            if (id != this.$route.query.uid) {
+                this.$router.push({
+                    path: '/visitor',
+                    query: {
+                        uid: id,
+                    }
+                })
+                location.reload()
+            }
+            else {
+                location.reload()
+            }
         },
         initEcharts() {
             // 基本柱状图
@@ -430,7 +463,7 @@ export default {
     }
 
     .name {
-        font-family: Montserrat;
+        font-family: Montserrat-Black;
         font-size: 35px;
         margin-bottom: 10px;
         width: 500px;
@@ -441,6 +474,7 @@ export default {
     }
 
     .institute {
+        font-family: Montserrat-Bold;
         font-size: 16px;
         margin-bottom: 10px;
         width: 500px;
@@ -449,7 +483,7 @@ export default {
 
     .motto {
         font-size: 16px;
-        font-style: italic;
+        font-family: Montserrat-Bold;
         margin-bottom: 10px;
         width: 500px;
     }
@@ -460,7 +494,7 @@ export default {
 }
 
 .el-button {
-    margin-left: 250px;
+    margin-left: auto;
 }
 
 .page-title {
@@ -482,8 +516,51 @@ export default {
     }
 }
 
+
+.left-col {
+    padding-top: 50px;
+    margin-left: 145px;
+    width: 600px;
+    height: 500px;
+    padding-right: 40px;
+}
+
+.right-col {
+    padding-top: 50px;
+    width: 600px;
+}
+
+.paper_name {
+    font-family: 'EB Garamond', serif;
+    margin-left: 20px;
+    color: black;
+    font-size: 35px;
+    font-weight: 800;
+    /* 设置初始状态字体为普通体 */
+    transition: color 0.3s ease-in-out, transform 0.2s ease-in-out;
+    /* 将多个属性的过渡效果放在同一个 'transition' 属性中 */
+    line-height: 2;
+    white-space: nowrap;
+    /* 设置不换行 */
+    overflow: hidden;
+    /* 超出部分隐藏 */
+    text-overflow: ellipsis;
+    /* 超出部分显示省略号 */
+}
+
+.paper_name:hover {
+    cursor: pointer;
+    transform: scale(1.05);
+    color: #0077ff !important;
+}
+
+.content {
+    padding-left: 20px;
+}
+
 .paper-list {
-    height: 420px;
+    height: 500px;
+    width: auto;
     overflow-y: auto;
     text-align: left !important;
 
@@ -536,38 +613,10 @@ export default {
                 flex-direction: row;
                 align-items: center;
 
-                .paper_name {
-                    font-family: 'EB Garamond', serif;
-                    margin-left: 20px;
-                    color: black;
-                    font-size: 35px;
-                    font-weight: 800;
-                    /* 设置初始状态字体为普通体 */
-                    transition: color 0.3s ease-in-out, transform 0.2s ease-in-out;
-                    /* 将多个属性的过渡效果放在同一个 'transition' 属性中 */
-                    line-height: 2;
-                    white-space: nowrap;
-                    /* 设置不换行 */
-                    overflow: hidden;
-                    /* 超出部分隐藏 */
-                    text-overflow: ellipsis;
-                    /* 超出部分显示省略号 */
-                }
-
-                .paper_name:hover {
-                    cursor: pointer;
-                    transform: scale(1.05);
-                    color: #0077ff !important;
-                }
-
                 .shrink {
                     margin-left: auto;
                 }
             }
-        }
-
-        .content {
-            padding-left: 20px;
         }
     }
 }
