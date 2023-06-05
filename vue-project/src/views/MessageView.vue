b3c46640116ccf26ce62ce3716144ff654ac6d54
Lord Turmoil <tony-turmoil@qq.com>
2023-06-05 17:14:31 +0800

    update: basic layout

--- vue-project/src/views/MessageView.vue
+++ vue-project/src/views/MessageView.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="msg-border">
         <div class="msg-wrapper" :style="scrollerHeight">
-            <div class="contact-wrapper round-border">
-                <div class="contact-nav">
+            <div class="contact-wrapper msg-box round-border">
+                <div class="contact-nav msg-box-nav">
                     <h3>Contacts</h3>
                 </div>
                 <div class="contact-scroller">
@@ -11,16 +11,60 @@
                             <span class="avatar"><img src="../assets/logo.png" /></span>
                             <span class="nickname">Tony Skywalker</span>
                         </div>
+                        <div class="contact-padding"></div>
                     </div>
                     <div class="contact-indicator"></div>
                 </div>
             </div>
-            <div class="message-wrapper round-border">
-                <div class="title">
+            <div class="message-wrapper msg-box round-border">
+                <div class="title msg-box-nav">
                     <h2>Tony Skywalker</h2>
-                    <span><i class="fa-solid fa-user"></i></span>
+                    <span><i class="fa-solid fa-user-xmark" title="Delete conversation"></i></span>
                 </div>
-                <div class="msg-panel"></div>
+                <div class="message-scroller">
+                    <div class="message-content">
+                        <div v-for="i in 10" class="message income">
+                            <div class="avatar"><img src="../assets/logo.png" /></div>
+                            <div class="payload">
+                                <div class="text">
+                                    <p>Hello there! I am a big fan of you! Can you talk to me? Anything? Please, I really
+                                        want to talk with you!</p>
+                                </div>
+                                <div class="time">
+                                    <p>2023-06-06 12:23:35</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="message income">
+                            <div class="avatar"><img src="../assets/logo.png" /></div>
+                            <div class="payload">
+                                <div class="text">
+                                    <p>Hello there!</p>
+                                </div>
+                                <div class="time">
+                                    <p>2023-06-06 12:23:35</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="message outcome">
+                            <div class="avatar"><img src="../assets/logo.png" /></div>
+                            <div class="payload">
+                                <div class="text">
+                                    <p>General Grievous!</p>
+                                </div>
+                                <div class="time">
+                                    <p>2023-06-06 12:23:35</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="message-input">
+                    <textarea ref="input" placeholder="Say something..." v-on:keyup="AutoGrowTextArea()"
+                        :style="textareaHeight"></textarea>
+                    <div class="clear but"><i class="fa-solid fa-eraser"></i></div>
+                    <div class="submit but"><i class="fa-regular fa-paper-plane" title="Send"></i></div>
+                </div>
             </div>
         </div>
     </div>
@@ -31,6 +75,9 @@ export default {
     data() {
         return {
             scrollerHeight: {
+                height: ''
+            },
+            textareaHeight: {
                 height: ''
             },
             scroller: null
@@ -59,7 +106,26 @@ export default {
     },
     methods: {
         resizeEventHandler() {
-            this.scrollerHeight.height = (window.innerHeight - 100) + 'px';
+            this.scrollerHeight.height = Math.max(window.innerHeight - 100, 400) + 'px';
+        },
+
+        // Auto-Grow-TextArea script.
+        // Script copyright (C) 2011 www.cryer.co.uk.
+        // Script is free to use provided this copyright header is included.
+        // Modified to Vue style by Tony.
+        AutoGrowTextArea() {
+            const input = this.$refs.input;
+            const clientHeight = input.clientHeight;
+            const scrollHeight = input.scrollHeight;
+            if (clientHeight < scrollHeight) {
+                this.textareaHeight.height = Math.min(Math.max(scrollHeight, 55), 200) + 'px';
+                if (clientHeight < scrollHeight) {
+                    this.textareaHeight.height = Math.min(Math.max(scrollHeight * 2 - clientHeight, 50), 200) + 'px';
+                }
+            }
+            if (input.value == '') {
+                this.textareaHeight.height = '55px';
+            }  
         }
     }
 }
