3e9c1a9e680ba3c618d6a5b6b7f52bd86a60fcb0
Lord Turmoil <tony-turmoil@qq.com>
2023-06-07 11:21:46 +0800

    feat: Mathjax support

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -5,28 +5,34 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-// 倒入全局样式表
+// 导入全局样式表
 import './assets/css/global.css'
+
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 import './assets/fonts/Fonts.css'
 
-
 // 配置请求
 import axios from 'axios'
-import VueHead from 'vue-head';
 // axios.defaults.baseURL='http://81.70.161.76:5000/api/v1/'
 axios.interceptors.request.use(config => {
     config.headers.Authorization = window.sessionStorage.getItem('token')
     return config
 })
 
-
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 Vue.prototype.$http = axios
 
+// Vue head injection
+import VueHead from 'vue-head';
 Vue.use(VueHead);
+
+// LaTeX dependency
+import MathJax, { initMathJax, renderByMathjax } from 'mathjax-vue'
+// initMathJax({}, onMathJaxReady);
+Vue.use(MathJax);
+
 
 new Vue({
     router,
@@ -38,18 +44,7 @@ new Vue({
             return {
                 inner: this.title
             }
-        },
-        script: [
-            // {
-            //     type: "application/javascript",
-            //     src: 'https://polyfill.io/v3/polyfill.min.js?features=es6',
-            // },
-            {
-                id: 'MathJax-script',
-                async: true,
-                src: 'https://cdn.jsdelivr.net/npm/mathjax/es5/tex-mml-chtml.min.js?config=TeX-AMS-MML_HTMLorMML'
-            }
-        ]
+        }
     }
 }).$mount('#app')
 
