3af2cbcc4f77ec55401bf81c5cf9221f9b51a4a8
YangBLUD <2433171628@qq.com>
2023-06-08 11:32:42 +0800

    Update index.js

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,52 +1,53 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Login from '../components/start/Login.vue'
-import Register from '../components/start/Register.vue'
-import Home from '../components/home.vue'
-import mainPage from '../components/mainPage.vue'
-import advSearch from '../components/search/advSearch.vue'
-import User from '../views/User.vue'
-import Info from '../views/UserInfo.vue'
-import Visitor from '../views/Visitor.vue'
-import Paper from '../views/Paper.vue'
-import Message from '../views/MessageView.vue'
-import PaperReview from '../views/PaperReviewPro.vue'
-import PaperUpload from '../views/PaperUpload.vue'
-import Admin from '../views/Admin.vue'
-import searchRes from '../components/search/searchRes.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../components/start/Login.vue";
+import Register from "../components/start/Register.vue";
+import Home from "../components/home.vue";
+import mainPage from "../components/mainPage.vue";
+import advSearch from "../components/search/advSearch.vue";
+import User from "../views/User.vue";
+import Info from "../views/UserInfo.vue";
+import Visitor from "../views/Visitor.vue";
+import Paper from "../views/Paper.vue";
+import Message from "../views/MessageView.vue";
+import PaperReview from "../views/PaperReviewPro.vue";
+import PaperUpload from "../views/PaperUpload.vue";
+import Admin from "../views/Admin.vue";
+import searchRes from "../components/search/searchRes.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', redirect: '/home' },
   {
-    path: '/home',
-    component: Home,
-    redirect: '/main',
-    children: [
-      { path: '/main', component: mainPage },
-      { path: '/advsearch', component: advSearch },
-      { path: '/user', component: User },
-      { path: '/info', component: Info },
-      { path: '/visitor', component: Visitor },
-      { path: '/paper', component: Paper },
-      { path: '/message', component: Message },
-      { path: '/paperreview', component: PaperReview},
-      { path: '/paperupload', component: PaperUpload},
-      { path: '/admin', component: Admin},
-      { path:'/searchres',component:searchRes}
-    ]
+    path: "/",
+    redirect: "/home",
   },
-  { path: '/login', component: Login },
-  { path: '/register', component: Register },
-]
+  {
+    path: "/home",
+    component: Home,
+    redirect: "/main",
+    children: [
+      { path: "/main", component: mainPage },
+      { path: "/advsearch", component: advSearch },
+      { path: "/user", component: User },
+      { path: "/info", component: Info },
+      { path: "/visitor", component: Visitor },
+      { path: "/paper", component: Paper },
+      { path: "/message", component: Message },
+      { path: "/paperreview", component: PaperReview },
+      { path: "/paperupload", component: PaperUpload },
+      { path: "/admin", component: Admin },
+      { path: "/searchres", component: searchRes },
+    ],
+  },
+  { path: "/login", component: Login },
+  { path: "/register", component: Register },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes,
-})
+});
 
-
-
-export default router
+export default router;
